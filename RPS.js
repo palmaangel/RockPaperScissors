d8af5ad8e2551fa63b8911d764c98cb97c46016a
Angel Palma <palmajangel@gmail.com>
2023-05-12 18:52:29 -0600

    added basic UI interface, will work on backend before finsihsing the front end

--- RPS.js
+++ RPS.js
@@ -1,6 +1,11 @@
 const PlayerChoice = prompt("Rock Paper or scissors?:")
 var computerChoice;
 var random_number= generateRandomNumber(1, 3);
+var result = null;
+
+//Rock.onclick = function buttonRock(){
+//    PlayerChoice = "rock"
+//}
 
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -19,28 +24,41 @@ else if (random_number == 3){
 
 if(PlayerChoice === computerChoice){
     console.log("its a tie!")
+    result = "its a tie!"
 }
 else if (PlayerChoice == "rock"){
     if (computerChoice == "paper"){ 
         console.log("You lose!")
+        result = "You lose!"
+
     }else {
     console.log("You wins!")
+    result = "You Win!"
     }
 }
 else if (PlayerChoice == "scissors"){
     if (computerChoice == "rock"){
         console.log("You lose!")
+        result = "You lose!"
     }else {
-    console.log("You wins!")
+        console.log("You wins!")
+        result = "You Win!"
     }
 }
 else if (PlayerChoice == "paper"){
     if (computerChoice == "scissors"){
         console.log("You lose!")
+        result = "You lose!"
+
     }else {
     console.log("You wins!")
+    result = "You Win!"
+
     }
 }
+document.getElementById("playerChoiceDisplay").innerHTML = PlayerChoice;
+document.getElementById("computerChoiceDisplay").innerHTML = computerChoice;
+document.getElementById("resultDisplay").innerHTML = result;
 
 console.log(PlayerChoice);
 console.log(computerChoice);
