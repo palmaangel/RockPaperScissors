7d7fa151bb2c34814b1db070c3d19b7361d3d358
Angel Palma <palmajangel@gmail.com>
2023-05-13 14:35:13 -0600

    tweaked some code

--- RPS.js
+++ RPS.js
@@ -1,7 +1,7 @@
 const PlayerChoice = prompt("Rock Paper or scissors?:")
 var computerChoice;
 var random_number= generateRandomNumber(1, 3);
-var result = null;
+var result;
 
 //Rock.onclick = function buttonRock(){
 //    PlayerChoice = "rock"
@@ -10,17 +10,9 @@ var result = null;
 function generateRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+const material = ["rock","paper","scissors"]
+computerChoice = material[random_number]
 
-
-if (random_number == 1){
-    computerChoice = "rock";
-}
-else if (random_number == 2){
-    computerChoice = "paper";
-}
-else if (random_number == 3){
-    computerChoice = "scissors";
-}
 
 if(PlayerChoice === computerChoice){
     console.log("its a tie!")
