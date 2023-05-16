a00246291e78025c1fda3c121a11d2ee01217089
Angel Palma <palmajangel@gmail.com>
2023-05-16 11:37:05 -0600

    added round function, need to figure out how to use it...

--- RPS.js
+++ RPS.js
@@ -1,51 +1,54 @@
-const PlayerChoice = prompt("Rock Paper or scissors?:")
+//const PlayerChoice = prompt("Rock Paper or scissors?:")
+var PlayerChoice;
 var computerChoice;
 var random_number= generateRandomNumber(1, 3);
 var result;
 
-//Rock.onclick = function buttonRock(){
-//    PlayerChoice = "rock"
-//}
+RockButtonDisplay.onclick = function buttonRock(){
+    PlayerChoice = "rock";
+    console.log(PlayerChoice);
+}
 
 function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * 3);
 }
 const material = ["rock","paper","scissors"]
 computerChoice = material[random_number]
 
-
-if(PlayerChoice === computerChoice){
-    console.log("its a tie!")
-    result = "its a tie!"
-}
-else if (PlayerChoice == "rock"){
-    if (computerChoice == "paper"){ 
-        console.log("You lose!")
-        result = "You lose!"
-
-    }else {
-    console.log("You wins!")
-    result = "You Win!"
+function gameRound(PlayerChoice,computerChoice){
+    if(PlayerChoice === computerChoice){
+        console.log("its a tie!")
+        result = "its a tie!"
     }
-}
-else if (PlayerChoice == "scissors"){
-    if (computerChoice == "rock"){
-        console.log("You lose!")
-        result = "You lose!"
-    }else {
+    else if (PlayerChoice == "rock"){
+        if (computerChoice == "paper"){ 
+            console.log("You lose!")
+            result = "You lose!"
+
+        }else {
         console.log("You wins!")
         result = "You Win!"
+        }
     }
-}
-else if (PlayerChoice == "paper"){
-    if (computerChoice == "scissors"){
-        console.log("You lose!")
-        result = "You lose!"
+    else if (PlayerChoice == "scissors"){
+        if (computerChoice == "rock"){
+            console.log("You lose!")
+            result = "You lose!"
+        }else {
+            console.log("You wins!")
+            result = "You Win!"
+        }
+    }
+    else if (PlayerChoice == "paper"){
+        if (computerChoice == "scissors"){
+            console.log("You lose!")
+            result = "You lose!"
 
-    }else {
-    console.log("You wins!")
-    result = "You Win!"
+        }else {
+        console.log("You wins!")
+        result = "You Win!"
 
+        }
     }
 }
 document.getElementById("playerChoiceDisplay").innerHTML = PlayerChoice;
